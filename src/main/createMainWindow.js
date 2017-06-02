32541c2e00c2795d87d057cc7022d5a5c45646e4
rentalname <agenda21st@gmail.com>
2017-06-02 17:13:16 +0900

    デバッグ環境の作成

--- src/main/createMainWindow.js
+++ src/main/createMainWindow.js
@@ -1,9 +1,13 @@
 import { BrowserWindow } from 'electron';
 
+const loadDevtool = require('electron-load-devtool');
+
 class MainWindow {
   constructor() {
     this.window = new BrowserWindow({ width: 800, height: 600 });
     this.window.loadURL(`file://${__dirname}/../../index.html`);
+    loadDevtool(loadDevtool.REACT_DEVELOPER_TOOLS);
+    this.window.webContents.openDevTools();
     this.window.on('closed', () => {
       this.window = null;
     });
