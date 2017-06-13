bfb9c663b4b33845ab5882c6194076ea372901d1
rentalname <agenda21st@gmail.com>
2017-06-13 22:52:39 +0900

    アプリケーションメニューのひな形を作成

--- src/main/index.js
+++ src/main/index.js
@@ -1,10 +1,29 @@
+/* eslint-disable no-unused-vars */
 import { app } from 'electron';
 import createMainWindow from './createMainWindow';
+import setAppMenu from './setAppMenu';
 
-let mainWindow = null;
+let mainWindow;
+
+function openFile() {
+  console.log('openFile');
+}
+
+function saveFile() {
+  console.log('saveFile');
+}
+
+function saveAsNewFile() {
+  console.log('saveAsNewFile');
+}
+
+function exportPDF() {
+  console.log('exportPDF');
+}
 
 app.on('ready', () => {
   mainWindow = createMainWindow();
+  setAppMenu({ openFile, saveFile, saveAsNewFile, exportPDF });
 });
 
 app.on('window-all-closed', () => {
